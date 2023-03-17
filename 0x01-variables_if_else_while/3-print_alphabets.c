151d0a240ff0756d7fe82c10d1b4f44d1e91e502
JosephMutua <josephmutua1738@gmail.com>
2023-03-17 18:41:45 +0000

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - The starting point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+	
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
