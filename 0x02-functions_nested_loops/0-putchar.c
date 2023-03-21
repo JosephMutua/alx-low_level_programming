47a4cb6200966fb45f7d4197ce238bd81ff1e3a9
JosephMutua <josephmutua1738@gmail.com>
2023-03-21 15:28:07 +0000

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "_putchar.c"
+/**
+ * main - The starting point of the execution
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This folder contains projects for the the 0x02.c - Functions, nested loops in ALX.
