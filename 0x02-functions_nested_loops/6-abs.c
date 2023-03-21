daf83f00368a0c319ac9969cca26ea0edbef6063
JosephMutua <josephmutua1738@gmail.com>
2023-03-21 19:41:12 +0000

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - determines the absolute value of num.
+ *
+ * Return: Returns the aboslute value.
+ */
+int _abs(int i)
+{
+	return (abs(i));
+}
---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * man - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int r;
+
+	 r = _abs(-1);
+	 printf("%d\n", r);
+}
