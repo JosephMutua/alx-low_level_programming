f19e3710245f39d18770a2453c228b2b89f78ca4
JosephMutua <josephmutua1738@gmail.com>
2023-03-21 19:43:15 +0000

    Task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 /**
  * _abs - determines the absolute value of num.
+ * @i: the number whose absolute value is being determined..
  *
  * Return: Returns the aboslute value.
  */
