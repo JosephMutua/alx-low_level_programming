d8eea33826c9b07de4a5aaf36681f4b91ea13b5a
JosephMutua <josephmutua1738@gmail.com>
2023-03-23 22:15:13 +0000

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - prints underscore n times.
+ * @n: number of times the underscore should be printed.
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
