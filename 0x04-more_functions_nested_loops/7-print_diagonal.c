50cfb5d4dbf5008be955c5c74ed690427f15b5bb
JosephMutua <josephmutua1738@gmail.com>
2023-03-23 22:58:16 +0000

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints the diagonal n times.
+ * @n: the number of times to draw the diagonal.
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i,j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+
+			for ( j = 0; j <= i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
