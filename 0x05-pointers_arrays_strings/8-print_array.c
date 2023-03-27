075e74d2441e20d526b7b6c53f37b2cbd5ef8774
JosephMutua <josephmutua1738@gmail.com>
2023-03-27 23:56:19 +0000

    Task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -5,14 +5,14 @@
  * print_array - prints the values of an array.
  * @a: the array to be printed.
  * @n: the size of the array
- * 
+ *
  * Return: void
  */
 void print_array(int *a, int n)
 {
 	int i;
-	
-	for (i = 0; i < n; i++)
+
+	for (i = 0; i <= n; i++)
 	{
 		if (i == n - 1)
 		{
