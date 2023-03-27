38237318da8b9429d5d8c8dbde94128159d6497c
JosephMutua <josephmutua1738@gmail.com>
2023-03-27 18:46:04 +0000

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - This changes the value of the address to 98.
+ * @*n: the pointer to the value to be changed.
+ *
+ * Return: 98.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is folder contains all the code for pointers and array in alx projects
