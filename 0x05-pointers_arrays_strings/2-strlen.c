b04d9ae6c362c428f23de0ec5438bf16c1a90d73
JosephMutua <josephmutua1738@gmail.com>
2023-03-27 19:25:26 +0000

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - determines the length of a string
+ * @s: the string to be determined its length
+ *
+ * Return: length of the string.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		i++;
+	}
+	return (i);
+}
