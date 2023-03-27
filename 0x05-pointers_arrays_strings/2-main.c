17a4640cbb050062a6da1169957ef9cfa38431e2
JosephMutua <josephmutua1738@gmail.com>
2023-03-27 19:51:34 +0000

    Task 2

--- 0x05-pointers_arrays_strings/2-main.c
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main - check the code
@@ -8,11 +9,14 @@
  */
 int main(void)
 {
-	char *str;
-	int len;
+	char *str, *str2;
+	int len, len2;
 
 	str = "My first strlen!";
 	len = _strlen(str);
 	printf("%d\n", len);
+	str2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+	len2 = _strlen(str2);
+	printf("%d\n", len2);
 	return (0);
 }
