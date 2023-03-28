5125c36b45ff59a543b1db0f53c7d44627234b08
JosephMutua <josephmutua1738@gmail.com>
2023-03-28 11:59:13 +0000

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copies a string to another destination
+ * @dest: the pointer to where the string is to be copied.
+ * @src: the string that is to be coppied. 
+ *
+ * Return: Always the string.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+	while (j <= i - 1)
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	return (dest);
+}
