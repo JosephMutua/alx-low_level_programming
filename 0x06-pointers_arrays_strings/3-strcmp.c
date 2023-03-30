1ed80452b7f997d123d57a0a232c771ca708e2f4
JosephMutua <josephmutua1738@gmail.com>
2023-03-30 05:52:55 +0000

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares who values are returns the difference
+ * @s1: the first value to be compared.
+ * @s2: the second value to be compared.
+ *
+ * Return: On success the difference.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1)
+	{
+		if (*s1 != *s2)
+			break;
+		s1++;
+		s2++;
+	}
+	return *(char *)s1 - *(char *)s2;
+}
