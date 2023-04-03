c80a5c5644439f0b59cf720770fe47c25fea7340
JosephMutua <josephmutua1738@gmail.com>
2023-04-03 19:58:19 +0000

    Task 2 creating a function for searching a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strchr - returns a pointer to the occurance of c in the s
+ * @s: string to be checked.
+ * @c: the letter to be checked.
+ *
+ * Return: On success return the pointer.
+ */
+char *_strchr(char *s, char c)
+{
+	
+	for (;*s != '\0'; s++)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+	}
+	if (c == '\0')
+	{
+		return (s);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
