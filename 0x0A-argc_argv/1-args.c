cbb89e1d7e9ad69a17e3515d74274b0fe4208333
JosephMutua <josephmutua1738@gmail.com>
2023-04-10 21:11:52 +0300

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of argv.
+ * @argc: the number of argv
+ * @argv: the list of arguments that passed in the command line.
+ *
+ * Return: O on success.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
