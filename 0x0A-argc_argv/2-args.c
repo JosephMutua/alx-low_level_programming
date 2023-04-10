12229b702f6e0f82cd30f28bd798263c81323d87
JosephMutua <josephmutua1738@gmail.com>
2023-04-10 21:27:49 +0300

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**main - prints all the arg vectors
+ * @argc: the number of argv
+ * @argv: an array of all arguements.
+ *
+ * Return: 0 on success.
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+
+	for (a = 0; a < argc; a++)
+	{
+		printf("%s\n", argv[a]);
+	}
+	return (0);
+}
