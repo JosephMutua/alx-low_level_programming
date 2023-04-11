b1eb3e411406920ea264ca00ef95747341d7e422
JosephMutua <josephmutua1738@gmail.com>
2023-04-12 00:59:19 +0300

    Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - finds the length of a string.
+ * @str - the length of the string.
+ *
+ * Return: length of the string
+ */
+int _strlen(char *str)
+{
+	unsigned int i;
+
+	i =  0;
+	while (*str != '\0')
+	{
+		i++;
+		str++;
+	}
+	return (i);
+}
+/**
+ * _strdup - returnts a copy of a string.
+ * @str: the string to be returned.
+ *
+ * Return: the str duplicate on success
+ */
+char *_strdup(char *str)
+{
+	char *copystr;
+	unsigned int len, i;
+
+	len = _strlen(str);
+	if (str == 0)
+		return (0);
+	copystr = (char *) malloc(sizeof(char) * len);
+	for (i = 0; i < len; i++)
+	{
+		copystr[i] = *str;
+		str++;
+	}
+	return (copystr);
+}
