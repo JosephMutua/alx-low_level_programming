4352a20cb5cdde1ba22fbcc827f1054a505a0948
JosephMutua <josephmutua1738@gmail.com>
2023-04-13 23:30:22 +0300

    Task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc -  locates memory to an array using malloc.
+ * @nmemb: the element of the array.
+ * @size: the size of the elements in the array
+ *
+ * Return: pointer to the array.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *i;
+	unsigned int j;
+
+	if (nmemb == 0)
+	{
+		return (NULL);
+	}
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	i = malloc(nmemb * size);
+	if (i == NULL)
+	{
+		return (NULL);
+	}
+
+	for (j = 0; j < nmemb; j++)
+	{
+		i[j] = 0;
+	}
+	return (i);
+}
