5ae7d7218e8d889a52a95a4f31f29c5ca68c247d
JosephMutua <josephmutua1738@gmail.com>
2023-04-13 23:55:36 +0300

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,11 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	int *col;
 	unsigned int j, fulsize;
 
-	if (nmemb == 0)
-	{
-		return (NULL);
-	}
-	if (size == 0)
+	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
