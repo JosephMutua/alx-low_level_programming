c14a79d090934630053cff56ae2a09c92306c57d
JosephMutua <josephmutua1738@gmail.com>
2023-04-13 21:16:57 +0300

    Task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strlen - finds the length of the string.
+ * @s: string to be determined.
+ *
+ * Return: the length of the string.
+ */
+unsigned int _strlen(char *s)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * string_nconcat - copies s2 n characters to s1.
+ * @s1: the string where s2 will be copied.
+ * @s2: the string to be copied
+ * @n: the number of string to be copied.
+ *
+ * Return: the string of the concantated string.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *constr;
+	unsigned int i;
+
+	constr = malloc(_strlen(s1) + n);
+	if (constr == NULL)
+		return (0);
+
+	for (i = 0; i < _strlen(s1) + n; i++)
+	{
+		if (i < _strlen(s1))
+		{
+			constr[i] = s1[i];
+		}
+		else
+		{
+			constr[i] = *s2;
+			s2++;
+		}
+	}
+	return (constr);
+}
