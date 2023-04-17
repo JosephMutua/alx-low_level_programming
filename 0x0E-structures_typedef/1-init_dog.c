9648e2f328dae5ff2d3b1705724f496e68a2fa84
JosephMutua <josephmutua1738@gmail.com>
2023-04-17 16:48:21 +0300

    Task 1 -correcting the conditional operator

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stddef.h>
 /**
  * init_dog - this function intialized the struct dog
  * @d: the sturct being intialized.
@@ -10,7 +11,14 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+	else
+	{
+		;
+	}
 }
