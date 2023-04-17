cf0fcc39ba1617cd76fb6e8b81856833346eefda
JosephMutua <josephmutua1738@gmail.com>
2023-04-17 17:48:20 +0300

    Task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,4 +17,18 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+ * struct dog_t - description of dog
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: Owner of the dog.
+ *
+ * Description: the description of dog's name, age, and owner
+ */
+struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+};
 #endif
