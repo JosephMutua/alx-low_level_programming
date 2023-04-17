bd7e534347b9729f8e3ec91263aff7cc0843fd09
JosephMutua <josephmutua1738@gmail.com>
2023-04-17 16:34:47 +0300

    Task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * init_dog - this function intialized the struct dog
+ * @d: the sturct being intialized.
+ * @name: the pointer to the name
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Return: Nothing.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
