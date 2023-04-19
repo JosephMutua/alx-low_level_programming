e5fb0bf54c24d2c055dbf9fd5490e3315382d403
JosephMutua <josephmutua1738@gmail.com>
2023-04-19 20:49:23 +0300

    Task 0: printing name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints the name in the arguement.
+ * @name: the name to be printed.
+ * @f: the pointer to the function
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f) (char *))
+{
+	(*f)(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+This folder contains the tasks for implementing function pointers in C ALX projects. 
