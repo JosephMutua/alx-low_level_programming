d11b75bc11346441892701efe9e082b6d5a80749
JosephMutua <josephmutua1738@gmail.com>
2023-04-20 21:13:29 +0300

    Task 1: printing numbers correcting the code style to fit betty

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,8 +16,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(values, n);
 
-	if (n <= 0)
-		return;
+	if (n == 0)
+		return (0);
 
 	for (i = 0; i < n; i++)
 	{
