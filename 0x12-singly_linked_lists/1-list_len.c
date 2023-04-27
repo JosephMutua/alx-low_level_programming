679b64ee8aefc5cc108995ba6c584a59488078fd
JosephMutua <josephmutua1738@gmail.com>
2023-04-27 16:27:41 +0300

    Task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "list.h"
+
+/**
+ * list_len - returns the number of elements in a list
+ * @h: the pointer to the list.
+ *
+ * Return: the number of elements in the list
+ */
+size_t list_len(const list_t *h)
+{
+	int i;
+
+	i = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
