dbd757ddf213aa6e4e95e892116266eaba8f6965
JosephMutua <josephmutua1738@gmail.com>
2023-04-27 20:04:22 +0300

    Update task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,5 @@
-#include "list.h"
+#include "lists.h"
+#include <stdlib.h>
 
 /**
  * add_node - adds node to the head
@@ -9,4 +10,21 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	int i;
+	list_t *node_t;
 
+	node_t = malloc(sizeof(list_t));
+	if (node_t == NULL)
+		return (NULL);
+	node_t->str = strdup(str);
+	i = 0;
+	while (*str != '\0')
+	{
+		i++;
+		str++;
+	}
+	node_t->len = i;
+	node_t->next = *head;
+	*head = node_t;
+	return (node_t);
+}
