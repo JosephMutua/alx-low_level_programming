f6a41fa3b6fcde073479a48d8c946ade45b8797d
JosephMutua <josephmutua1738@gmail.com>
2023-05-10 16:35:57 +0300

    Task 2 correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fl = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fl = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fl < 0)
 		return (-1);
 	len = 0;
